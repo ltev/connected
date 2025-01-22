62758d971d8bccf3897e09e0efadd3967945eae2
ltev <ltev@interia.pl>
2025-01-22 18:01:27 +0100

    Added custom userData DataSource configuration

--- connected-data/src/main/java/com/ltev/connected/config/ConnectedDatabaseConfiguration.java
+++ connected-data/src/main/java/com/ltev/connected/config/ConnectedDatabaseConfiguration.java
@@ -1,14 +1,22 @@
 package com.ltev.connected.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "com.ltev.connected.repository.main",
+        entityManagerFactoryRef = "connectedEntityManagerFactory", transactionManagerRef = "connectedTransactionManager")
 public class ConnectedDatabaseConfiguration {
 
     @Bean
@@ -18,9 +26,25 @@ public class ConnectedDatabaseConfiguration {
     }
 
     @Bean
-    public DataSource connectedDataSource(DataSourceProperties connectedDataSourceProperties) {
+    public DataSource connectedDataSource(@Qualifier("connectedDataSourceProperties") DataSourceProperties connectedDataSourceProperties) {
         return connectedDataSourceProperties.initializeDataSourceBuilder()
                 .type(HikariDataSource.class)
                 .build();
+    }
+
+    @Bean//("entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean connectedEntityManagerFactory(
+            @Qualifier("connectedDataSource") DataSource userDataDataSource,
+            EntityManagerFactoryBuilder builder) {
+        return builder.dataSource(userDataDataSource)
+                .packages("com.ltev.connected.domain")
+                .persistenceUnit("connected")
+                .build();
+    }
+
+    @Bean//("transactionManager")
+    public PlatformTransactionManager connectedTransactionManager(
+            LocalContainerEntityManagerFactoryBean connectedEntityManagerFactoryBean) {
+        return new JpaTransactionManager(connectedEntityManagerFactoryBean.getObject());
     }
 }
